70370f643cea7e1c3b6fe584b32a085dc15d6f20
Shaadan <s_elshin@mail.ru>
2026-04-15 15:39:12 +0300

    feat: add initial migration with all tables + fix broken revision chain

--- backend/alembic/versions/20260414_0001_autoservices_fields.py
+++ backend/alembic/versions/20260414_0001_autoservices_fields.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "20260414_0001"
-down_revision = "0913547ae571"
+down_revision = "0001_initial"
 branch_labels = None
 depends_on = None
 
